2f3853b078a1b600b8228526bd690f263978abda
kamo <kamominle@gmail.com>
2025-03-12 10:28:07 +0900

    タグマネージャー設置

--- app/layout.js
+++ app/layout.js
@@ -31,9 +31,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-5NHDQB8L'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NHDQB8L"
+            height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
+        </noscript>
         {children}
       </body>
     </html>
