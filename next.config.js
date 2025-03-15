508aa9a2a3154604fc1b8889ec9f4b75bb8c643b
kamo <kamominle@gmail.com>
2025-03-15 21:02:13 +0900

    レスポンスヘッダーの設定を追加

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, s-maxage=3600, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
+};
