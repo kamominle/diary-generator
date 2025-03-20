934519d03a91e7c05251378ac5d36152732a2d73
kamo <kamominle@gmail.com>
2025-03-20 11:30:17 +0900

    サイトロゴを画像に変更

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,15 @@
+import Image from 'next/image';
+
 export default function Header() {
-    return (
-      <header className="p-4 bg-white shadow">
-        <div className="container mx-auto">
-          <h1 className="text-xl font-bold text-gray-800">
-            <a href="/">代筆くん</a>
-          </h1>
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className="p-4 bg-white shadow">
+      <div className="container mx-auto">
+        <h1 className="text-xl font-bold text-gray-800">
+          <a href="/" className="flex md:justify-start justify-center">
+            <Image src="/daihitsukun-logo.png" alt="代筆くん" width={250} height={60} />
+          </a>
+        </h1>
+      </div>
+    </header>
+  );
+}
