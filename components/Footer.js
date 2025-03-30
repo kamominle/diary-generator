40d9ee5032d6d0928bfb22aa101454a1babb78c9
kamo <kamominle@gmail.com>
2025-03-30 11:58:29 +0900

    フッター公式X導線

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,8 @@ export default function Footer() {
           <div className="text-sm text-gray-500">
             <a href="/request" className="hover:text-gray-500 mr-6">機能改善のご要望</a>
             <a href="/contact" className="hover:text-gray-500 mr-6">お問い合わせ</a>
-            <a href="/privacy" className="hover:text-gray-500">プライバシーポリシー</a>
+            <a href="/privacy" className="hover:text-gray-500 mr-6">プライバシーポリシー</a>
+            <a href="https://x.com/ai_hituji" target="_blank" rel="noopener noreferrer" className="hover:text-gray-500">公式X</a>
           </div>
         </div>
       </footer>
